055a719ab887ac4fea435fd1a0e826b5eb23a928
MEDA ANIL KUMAR <anilkumar.meda@temenos.com>
2022-07-18 13:55:11 +0530

    Update index.js
    updated

--- index.js
+++ index.js
@@ -9,12 +9,29 @@ btn.addEventListener('click',execute)
 function execute(){
     r1=first.value*second.value
     r2=second.value*third.value
-    console.log(r1,r2)
-    res=Math.abs(r1-r2)
-    if(r1>r2){
-        output.textContent=`<h1>loss of ${res} and percent ${res}%</h1>`
+    // console.log(r1,r2)
+    // res=Math.abs(r1-r2)
+    // if(r1>r2){
+    //     output.textContent=`<h1>loss of ${res} and percent ${res}%</h1>`
+    // }
+    // else{
+    //     output.textContent=`Profit of ${res} and percent ${res}%`
+    // }
+    if(r2-r1>0){
+        var profit=r2-r1
+        var profitper=(profit/(r1)*100)
+        output.style.color = "green";
+        output.innerHTML=`<h1>Profit of ${profit} and percent ${profitper}%</h1>`
+    }
+    else if(r2-r1<0){
+        var loss=r1-r2
+        var lossper=(loss/(r1)*100)
+        output.style.color = "red";
+        output.innerHTML=`<h1>loss of ${loss} and percent ${lossper}%</h1>`
     }
     else{
-        output.textContent=`Profit of ${res} and percent ${res}%`
+        output.style.color = "white";
+        output.innerText = "No Pain No Gain"
+
     }
 }
